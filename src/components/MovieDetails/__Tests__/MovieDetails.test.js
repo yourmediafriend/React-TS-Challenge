743e0d28d8c08f0c748a57e0c8c719b15e1160aa
John Walker <yourmediafriend@gmail.com>
2022-09-15 09:05:53 +0100

    Updating test

--- src/components/MovieDetails/__Tests__/MovieDetails.test.js
+++ src/components/MovieDetails/__Tests__/MovieDetails.test.js
@@ -2,9 +2,7 @@ describe(`Movie Details`, () => {
 	test.todo("Fetches a Movie based on ID from TMDB API");
 	test.todo("Has Movie Title");
 	test.todo("Has Movie Image");
-	test.todo("Has Movie Long Decription");
+	test.todo("Has Movie Overview");
 	test.todo("Has Movie Date");
 	test.todo("Has Movie Genre");
-	test.todo("Has Trailer");
-	test.todo("Has Cast");
 });
